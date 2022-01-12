9b649ff0d3e9925c79874c18fe5cc16d22223fd9
ali mohsen <ali.mhsen@gmail.com>
2022-01-12 23:54:40 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+var d_l_maode = document.getElementById("d_l_maode");
+/* d_l_maode.addEventListener("onclick", function () {
+    var body = document.getElementsByTagName("body");
+    var title = document.getElementsByTagName("h1");
+    var paragraph = document.getElementsByTagName("p");
+    body.style.backgroundColor = "black";
+}, false);
+function reg() {
+    var body = document.getElementsByTagName("body");
+    var title = document.getElementsByTagName("h1");
+    var paragraph = document.getElementsByTagName("p");
+
+    body.style.backgroundColor = "black";
+} */
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    }
