233f99cf400e1b7a0af231376c07fb77d297755f
ali mohsen <ali.mhsen@gmail.com>
2022-01-13 01:30:36 +0300

    first commit5

--- js/app.js
+++ js/app.js
@@ -16,4 +16,19 @@ function reg() {
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark");
+    var moon = document.getElementById("moon1");
+    var sun = document.getElementById("sun1");
+    if(element.className=="dark"){
+        moon.classList.remove("show");
+        moon.classList.toggle("haide");
+        sun.classList.remove("haide");
+        sun.classList.toggle("show");
+        
+    }else if(element.className != "dark"){
+        sun.classList.remove("show");
+        sun.classList.toggle("haide");
+        moon.classList.remove("haide");
+        moon.classList.toggle("show");
+       
+    }
     }
